b9fe99254f710dadff20085b035e8f8e02b3c4e5
Seamus Q O'Malley <63891206+seamus0192@users.noreply.github.com>
2023-01-23 11:23:27 -0800

    Add files via upload

--- scatter.R
+++ scatter.R
@@ -6,7 +6,19 @@ dt1<-read.csv("mlb_players_18.csv")
 dt2<-data.frame(dt1)
 
 
-smoothScatter(dt2$H, dt2$AVG, main="Scatterplot Example", 
-     xlab="H", ylab="AVG", type = "p",pch=2)
+sub.categories = split(dt2,dt2$position)
+A = numeric(10)
+i = 1
+for (category in sub.categories) {
+  pos.avg = mean(category$AVG)
+  A[i] = pos.avg
+  i = i + 1
+}
+
+
+smoothScatter(dt2$AB, dt2$HR, main="Scatterplot Example", 
+              xlab="H", ylab="AVG",nrpoints = Inf)
+
+barplot(A,names.arg = c("1B","3B","2B","C","CF","DH","LF","P","SS","RF"))
 
 
